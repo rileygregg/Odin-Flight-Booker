a2114d496c761e992751b182efe4e3f6c3fc6372
Riley Gregg <rtgregg93@gmail.com>
2023-05-23 19:44:00 -0400

    Update search form to add passenger count

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -4,6 +4,7 @@ class FlightsController < ApplicationController
     #   @flights = Flight.where(departure_airport_id: params[:departure_airport_id])
     # else
       @flights = Flight.all
+      passenger_count = params[:number_of_passengers] || 1
     # end
   end
 
--- app/helpers/flights_helper.rb
+++ app/helpers/flights_helper.rb
@@ -1,4 +1,5 @@
 module FlightsHelper
+  # formats start_datetime to show full month, date in integer, full year
   def start_date_formatted(flight)
     flight.start_datetime.strftime("%B %d, %Y")
   end
