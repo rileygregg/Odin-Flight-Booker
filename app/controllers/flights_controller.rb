f69f43aa6094bc6d603536abea4085be67d617ac
Riley Gregg <rtgregg93@gmail.com>
2023-05-23 19:45:12 -0400

    Clean up flight controller

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,11 +1,6 @@
 class FlightsController < ApplicationController
   def index
-    # if params[:departure_airport_id].present?
-    #   @flights = Flight.where(departure_airport_id: params[:departure_airport_id])
-    # else
       @flights = Flight.all
-      passenger_count = params[:number_of_passengers] || 1
-    # end
   end
 
 end
