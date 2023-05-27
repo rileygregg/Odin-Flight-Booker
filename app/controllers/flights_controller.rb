e85c99e90f37c89fe04b036a4db41f5812f946f0
Riley Gregg <rtgregg93@gmail.com>
2023-05-27 15:20:00 -0400

    Update views on index to show results

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,6 +1,12 @@
 class FlightsController < ApplicationController
   def index
       @flights = Flight.all
+      @departure_param = params[:departure_airport_id]
+      @arrival_param =   params[:arrival_airport_id]
+      @number_of_passengers_param = params[:number_of_passengers]
+      @departure_date_param = params[:start_datetime]
+      @key = params[:commit]
+
   end
 
 end
